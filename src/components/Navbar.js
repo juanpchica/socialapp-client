89bc9654f5ce3c7dbf884f4bc55827a000e8df96
juanpchica <juanpchica@hotmail.com>
2021-06-08 17:41:49 -0500

    Navbar component added

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <Button color='inherit'>Home</Button>
+        <Button color='inherit'>Login</Button>
+        <Button color='inherit'>Signup</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
