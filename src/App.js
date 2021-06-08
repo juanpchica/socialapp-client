8bf9d8e8dd717605d4b4228d7e0e4ee76960757a
juanpchica <juanpchica@hotmail.com>
2021-06-08 17:52:08 -0500

    Navbar and router dom added to the project with the main components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { Navbar } from "./components/Navbar";
 function App() {
-  return <div className='App'>Works</div>;
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Route exact component={Home} path='/' />
+        <Route component={Login} path='/login' />
+        <Route component={Signup} path='/signup' />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
