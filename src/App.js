51aa354933a5704895a5ce631a44aa86fef0d945
juanpchica <juanpchica@hotmail.com>
2021-06-09 16:08:23 -0500

    Mui Theme to handle styles added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { Navbar } from "./components/Navbar";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ff4400",
+    },
+    secondary: {
+      light: "#0066ff",
+      main: "#0044ff",
+      contrastText: "#ffcc00",
+    },
+  },
+});
+
 function App() {
   return (
-    <div>
+    <MuiThemeProvider theme={theme}>
       <Router>
         <Navbar />
         <Route exact component={Home} path='/' />
         <Route component={Login} path='/login' />
         <Route component={Signup} path='/signup' />
       </Router>
-    </div>
+    </MuiThemeProvider>
   );
 }
 
