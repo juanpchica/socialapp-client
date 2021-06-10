bb59755e718557b19dad8cd858c4a2e3778f937f
juanpchica <juanpchica@hotmail.com>
2021-06-10 16:57:10 -0500

    Showing first screams in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,23 +3,35 @@ import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 
 export const Home = () => {
-  const [screams, setScreams] = useState([]);
+  const [screams, setScreams] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   const getScreams = async () => {
-    const response = await axios.get("/screams");
-    const data = await response.json();
-    console.log(data);
+    const { data } = await axios.get(`/screams`);
+
+    //Set screams and show them
+    setScreams(data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getScreams();
   }, []);
 
-  let screamsMarkup;
+  let recentScreamsMarkup;
+
+  if (screams && !isLoading) {
+    recentScreamsMarkup = screams.map((scream, index) => {
+      return <p key={index}>{scream.body}</p>;
+    });
+  } else {
+    recentScreamsMarkup = <p>Loading....</p>;
+  }
+
   return (
-    <Grid container spacing={16}>
+    <Grid container spacing={8}>
       <Grid item xs={12} sm={8}>
-        Scream list
+        {recentScreamsMarkup}
       </Grid>
       <Grid item xs={12} sm={4}>
         Profile
