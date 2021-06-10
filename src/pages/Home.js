d0cdb0741c331dff35f64c5a711f3aaa62c74ad5
juanpchica <juanpchica@hotmail.com>
2021-06-10 16:30:12 -0500

    Fetching screams from server and console log

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
+import axios from "axios";
 
 export const Home = () => {
+  const [screams, setScreams] = useState([]);
+
+  const getScreams = async () => {
+    const response = await axios.get("/screams");
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getScreams();
+  }, []);
+
+  let screamsMarkup;
   return (
     <Grid container spacing={16}>
       <Grid item xs={12} sm={8}>
