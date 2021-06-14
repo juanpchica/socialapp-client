f4a1b89a2b035fa52a1340ac8ed5671799843584
juanpchica <juanpchica@hotmail.com>
2021-06-14 17:08:25 -0500

    Image user and console data from scream

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export const Scream = ({ scream }) => {
+  console.log(userImage);
   return (
     <div>
       <h1>{scream.body}</h1>
-      <img src={scream.imgUrl} />
+      <img src={scream.userImage} />
     </div>
   );
 };
