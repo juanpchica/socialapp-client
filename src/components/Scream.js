7fb8bbf5614c873346cb7773acebcb8e19905840
juanpchica <juanpchica@hotmail.com>
2021-06-13 21:10:04 -0500

    Scream image and title added

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export const Scream = ({ scream }) => {
-  return <div>{scream.body}</div>;
+  return (
+    <div>
+      <h1>{scream.body}</h1>
+      <img src={scream.imgUrl} />
+    </div>
+  );
 };
