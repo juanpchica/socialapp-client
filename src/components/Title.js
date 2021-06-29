5bf2c3737fbe7bcdb05ec2483762666515646c2b
juanpchica <juanpchica@hotmail.com>
2021-06-29 09:26:17 -0500

    Breadcrumbs added to the title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export const Title = () => {
-  return <div>New title</div>;
+  return (
+    <div>
+      <Breadcrumbs aria-label='breadcrumb'>
+        <Link color='inherit' href='/' onClick={handleClick}>
+          Material-UI
+        </Link>
+        <Link
+          color='inherit'
+          href='/getting-started/installation/'
+          onClick={handleClick}
+        >
+          Core
+        </Link>
+        <Typography color='textPrimary'>Breadcrumb</Typography>
+      </Breadcrumbs>
+    </div>
+  );
 };
