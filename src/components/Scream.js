e64b0ff2e972a3042f4212f37a3a0f77f9a7f8ea
juanpchica <juanpchica@hotmail.com>
2021-06-30 14:01:28 -0500

    withStyle component scream added

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,9 +1,19 @@
 import React from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+const styles = {
+  card: {
+    display: "flex",
+  },
+};
 
-export const Scream = ({ scream }) => {
+const Scream = (props) => {
+  const { classes } = props;
+
   return (
     <div>
       <h1>{scream.body}</h1>
     </div>
   );
 };
+
+export default withStyles(styles)(Scream);
