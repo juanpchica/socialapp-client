337416cac91dc0ab8ccbdc78dd6af5f903e0fb46
juanpchica <juanpchica@hotmail.com>
2021-07-13 16:41:08 -0500

    Refactoring theme for styling

--- src/App.js
+++ src/App.js
@@ -3,51 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import themeFile from "./util/theme";
 
 import { Home } from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { Navbar } from "./components/Navbar";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#ff4400",
-    },
-    secondary: {
-      light: "#0066ff",
-      main: "#0044ff",
-      contrastText: "#ffcc00",
-    },
-  },
-  spreadThis: {
-    form: {
-      textAlign: "center",
-    },
-    icon: {
-      maxWidth: "100px",
-      margin: "20px auto",
-    },
-    pageTitle: {
-      magin: "10px auto",
-    },
-    textField: {
-      margin: "10px auto",
-    },
-    button: {
-      marginTop: 20,
-      position: "relative",
-    },
-    customError: {
-      color: "red",
-      fontSize: "0.8rem",
-      marginTop: 10,
-    },
-    progress: {
-      position: "absolute",
-    },
-  },
-});
+const theme = createMuiTheme(themeFile);
 
 function App() {
   return (
---
+++ src/util/theme.js
@@ -0,0 +1,39 @@
+export default {
+  palette: {
+    primary: {
+      main: "#ff4400",
+    },
+    secondary: {
+      light: "#0066ff",
+      main: "#0044ff",
+      contrastText: "#ffcc00",
+    },
+  },
+  spreadThis: {
+    form: {
+      textAlign: "center",
+    },
+    icon: {
+      maxWidth: "100px",
+      margin: "20px auto",
+    },
+    pageTitle: {
+      magin: "10px auto",
+    },
+    textField: {
+      margin: "10px auto",
+    },
+    button: {
+      marginTop: 20,
+      position: "relative",
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10,
+    },
+    progress: {
+      position: "absolute",
+    },
+  },
+};
