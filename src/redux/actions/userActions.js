4936512ffb841fb1dd0d689373170c71f8948518
juanpchica <juanpchica@hotmail.com>
2021-07-19 08:41:57 -0500

    User action for login

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,1 +1,21 @@
 //USER LOGIN
+import { SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
+import axios from "axios";
+
+export const loginUser = (userData, history) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/login", userData)
+    .then((res) => {
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+};
