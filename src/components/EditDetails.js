0b340eefe8cd42da85af28402278a4a65018f723
juanpchica <juanpchica@hotmail.com>
2021-08-05 14:11:10 -0500

    Setting state when mountComponent

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 //Mui
 import withStyles from "@material-ui/core/styles/withStyles";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+
+//Icons
+import EditIcon from "@material-ui/icons/Edit";
 
 //Redux
 import { connect } from "react-redux";
@@ -9,13 +15,37 @@ import { editUserDetails, getUserData } from "../redux/actions/userActions";
 
 const styles = (theme) => ({
   ...theme.spreatThis,
+  button: {
+    float: "right",
+  },
 });
 
 class EditDetails extends Component {
+  state = {
+    bio: "",
+    website: "",
+    location: "",
+    open: false,
+  };
+
+  componentDidMount() {
+    const { credentials } = this.props;
+    this.setState({
+      bio: credentials.bio ? credentials.bio : "",
+      website: credentials.website ? credentials.website : "",
+      location: credentials.location ? credentials.location : "",
+    });
+  }
+
   render() {
     const { classes } = this.props;
-
-    return <div className={classes.container}>hello</div>;
+    return (
+      <Tooltip title='Edit User' className={classes.button}>
+        <IconButton>
+          <EditIcon />
+        </IconButton>
+      </Tooltip>
+    );
   }
 }
 
@@ -26,6 +56,13 @@ const mapPropsToState = (state) => ({
 const mapPropsToActions = {
   editUserDetails,
   getUserData,
+};
+
+EditDetails.propTypes = {
+  credentials: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  editUserDetails: PropTypes.func.isRequired,
+  getUserData: PropTypes.func.isRequired,
 };
 
 export default connect(
