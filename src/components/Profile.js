2915edfa5ab49175479781a84c13b54211a74bd1
juanpchica <juanpchica@hotmail.com>
2021-08-05 08:05:01 -0500

    Adding html structure for profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
+import { Link } from "react-router-dom";
 
 //Redux
 import { connect } from "react-redux";
+
+//Mui
+import Paper from "@material-ui/core/Paper";
+import MuiLink from "@material-ui/core/Link";
 
 const styles = (theme) => ({
   ...theme.spreadThis,
@@ -16,13 +21,37 @@ class Profile extends Component {
       user: {
         authenticated,
         credentials: { handle, createdAt, imageUrl, bio, website, location },
-        loading
+        loading,
       },
     } = this.props;
 
-    let profileMarkup = !loading ?( authenticated ? (
-        <Paper></Paper>
-    ):()):(<p>Loading...</p>);
+    let profileMarkup = !loading ? (
+      authenticated ? (
+        <Paper className={classes.paper}>
+          <div className={classes.profile}>
+            <div className='profile-image'>
+              <img src={imageUrl} alt='Profile' />
+            </div>
+            <hr />
+            <div className='profile-details'>
+              <MuiLink
+                component={Link}
+                to={`/users/${handle}`}
+                color='primary'
+                variant='h5'
+              >
+                @{handle}
+              </MuiLink>
+              <hr />
+            </div>
+          </div>
+        </Paper>
+      ) : (
+        ""
+      )
+    ) : (
+      <p>Loading...</p>
+    );
 
     return <div></div>;
   }
