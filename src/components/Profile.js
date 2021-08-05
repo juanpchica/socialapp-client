4b87713026a4e959a59a7a6aeebebda5c278a97b
juanpchica <juanpchica@hotmail.com>
2021-08-05 08:38:22 -0500

    Ui icons installed and profile view finished

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
 
 //Redux
 import { connect } from "react-redux";
@@ -11,6 +12,10 @@ import Paper from "@material-ui/core/Paper";
 import MuiLink from "@material-ui/core/Link";
 import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
+import LinkIcon from "@material-ui/icons/Link";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
 import { Fragment } from "react";
 
 const styles = (theme) => ({
@@ -46,22 +51,55 @@ class Profile extends Component {
                 @{handle}
               </MuiLink>
               <hr />
-
               {bio && <Typography variant='body2'>{bio}</Typography>}
               <hr />
               {location && (
                 <Fragment>
-                  <LocationOn color='primary'>
+                  <LocationOnIcon color='primary'>
                     <span>{location}</span>
-                  </LocationOn>
+                  </LocationOnIcon>
                   <hr />
                 </Fragment>
               )}
+              {website && (
+                <Fragment>
+                  <LinkIcon>
+                    <a href={website} target='_blank' rel='noopener noreferrer'>
+                      {" "}
+                      {website}
+                    </a>
+                  </LinkIcon>
+                </Fragment>
+              )}
+              <CalendarTodayIcon color='primary' />{" "}
+              <span>Joined {dayjs(createdAt).format("MMM YYYY")} </span>
             </div>
           </div>
         </Paper>
       ) : (
-        ""
+        <Paper className={classes.paper}>
+          <Typography variant='body2' align='center'>
+            No profile found, please login again
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              variant='contained'
+              color='primary'
+              component={Link}
+              to='/login'
+            >
+              Login
+            </Button>
+            <Button
+              variant='contained'
+              color='secondary'
+              component={Link}
+              to='/signup'
+            >
+              Signup
+            </Button>
+          </div>
+        </Paper>
       )
     ) : (
       <p>Loading...</p>
