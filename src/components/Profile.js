ccd9d9b0a593e3dd3c6cc6719b8e6de52c86fea8
juanpchica <juanpchica@hotmail.com>
2021-08-05 07:38:42 -0500

    Profile adding connect for redux and classes style

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,10 +2,26 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 
+//Redux
+import { connect } from "react-redux";
+
+const styles = (theme) => ({
+  ...theme.spreadThis,
+});
+
 class Profile extends Component {
   render() {
     return <div></div>;
   }
 }
 
-export default Profile;
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+Profile.PropTypes = {
+  user: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps)(withStyles(styles)(Profile));
