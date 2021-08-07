0420566ec101669b4dff80cbfd34d771fb2649df
juanpchica <juanpchica@hotmail.com>
2021-08-07 14:04:28 -0500

    Adding icons and redux to LikeButton componen

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,9 +1,51 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+//Redux
+import { connect } from "react-redux";
+import { likeScream, unlikeScream } from "../redux/actions/dataActions";
+import MyButton from "../util/MyButton";
+
+// Icons
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
 class LikeButton extends Component {
   render() {
-    return <div></div>;
+    const {
+      user: { authenticated, likes },
+    } = this.props;
+
+    //const buttonMarkup = (authenticated)? :
+
+    return (
+      <div>
+        <MyButton tip='Like'>
+          <FavoriteBorder color='primary' />
+        </MyButton>
+        <MyButton tip='Undo like'>
+          <FavoriteIcon color='primary' />
+        </MyButton>
+      </div>
+    );
   }
 }
 
-export default LikeButton;
+LikeButton.propTypes = {
+  user: PropTypes.object.isRequired,
+  likeScream: PropTypes.func.isRequired,
+  unlikeScream: PropTypes.func.isRequired,
+};
+
+const mapPropsToState = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapPropsToActions = {
+  likeScream,
+  unlikeScream,
+};
+
+export default connect(mapPropsToState, mapPropsToActions)(LikeButton);
