185d5ddfc6b60f4b9b8e680da6fcfb32aa2ef570
juanpchica <juanpchica@hotmail.com>
2021-08-07 15:18:57 -0500

    Styles and libraries added in delete button component

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
+import { Fragment } from "react";
+import MyButton from "../util/MyButton";
+
+//Redux
+import { connect } from "react-redux";
+
+//Mui
+import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+
+//Icons
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+
+const styles = (theme) => ({
+  ...theme.spreatThis,
+  button: {
+    float: "right",
+  },
+});
 
 class DeleteButton extends Component {
   render() {
-    return <div></div>;
+    return (
+      <Fragment>
+        <MyButton tip='Delete Scream' onClick={this.handleOpen}></MyButton>
+      </Fragment>
+    );
   }
 }
 
-export default DeleteButton;
+export default withStyles(styles)(DeleteButton);
