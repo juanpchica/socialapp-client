b7a31625208c863ee0110873f7ce373bee1f09dc
juanpchica <juanpchica@hotmail.com>
2021-08-07 15:34:38 -0500

    Adding Dialog to DeleteButton component

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -4,14 +4,12 @@ import MyButton from "../util/MyButton";
 
 //Redux
 import { connect } from "react-redux";
-
+import { deleteScream } from "../redux/actions/dataActions";
 //Mui
 import withStyles from "@material-ui/core/styles/withStyles";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
 
 //Icons
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
@@ -24,10 +22,26 @@ const styles = (theme) => ({
 });
 
 class DeleteButton extends Component {
+  state = {
+    open: false,
+  };
+
   render() {
     return (
       <Fragment>
-        <MyButton tip='Delete Scream' onClick={this.handleOpen}></MyButton>
+        <MyButton tip='Delete Scream' onClick={this.handleOpen}>
+          <DeleteOutlineIcon color='secondary' />
+        </MyButton>
+
+        <Dialog open={this.state.open}>
+          <DialogTitle>
+            Are you sure you want to delete this scream ?
+          </DialogTitle>
+          <DialogActions>
+            <Button>Cancel</Button>
+            <Button>Delete</Button>
+          </DialogActions>
+        </Dialog>
       </Fragment>
     );
   }
