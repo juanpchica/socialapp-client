1b5bb801203d0df977f04a5d41a2f6e5c82563a9
juanpchica <juanpchica@hotmail.com>
2021-08-21 15:09:41 -0500

    Icon and libraries for scream Dialog added

--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+//MUI
+import { Dialog } from "@material-ui/core";
+
+//My Components
+import MyButton from "../util/MyButton";
+
+//Icons
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
 
 const ScreamDialog = () => {
-  return <div>scream</div>;
+  return (
+    <Fragment>
+      <MyButton tip='Expand Scream!!!'>
+        <UnfoldMoreIcon color='primary' />
+      </MyButton>
+      <Dialog></Dialog>
+    </Fragment>
+  );
 };
 
 export default ScreamDialog;
