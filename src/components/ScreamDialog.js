33381f189b89f229a262f0767f653494a4ccf247
juanpchica <juanpchica@hotmail.com>
2021-08-21 16:09:19 -0500

    Adding info of scream and styling

--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -1,30 +1,73 @@
 import React, { Fragment, useState } from "react";
 import PropTypes from "prop-types";
+import dayjs from "dayjs";
+import { Link } from "react-router-dom";
 
 //MUI
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import withStyles from "@material-ui/core/styles/withStyles";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 //My Components
 import MyButton from "../util/MyButton";
+import LikeButton from "./LikeButton";
 
 //Icons
+import CloseIcon from "@material-ui/icons/Close";
 import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import ChatIcon from "@material-ui/icons/Chat";
 
 //Redux
 import { connect } from "react-redux";
 import { getScream } from "../redux/actions/dataActions";
 
-const styles = {
+const styles = (theme) => ({
+  ...theme.spreatThis,
+  profileImage: {
+    width: 200,
+    height: 200,
+    borderRadius: "50%",
+    objectFit: "cover",
+  },
+  dialogContent: {
+    padding: 20,
+  },
+  closeButton: {
+    position: "absolute",
+    left: "90%",
+  },
   buttonDialog: {
     position: "absolute",
-    right: "15px",
+    left: "90%",
   },
-};
+  spinnerDiv: {
+    textAlign: "center",
+    marginTop: 50,
+    marginBottom: 50,
+  },
+  invisibleSeparator: {
+    border: "none",
+  },
+});
 
-const ScreamDialog = ({ classes, screamId, getScream, UI: { loading } }) => {
+const ScreamDialog = ({
+  classes,
+  screamId,
+  getScream,
+  UI: { loading },
+  scream: {
+    body,
+    createdAt,
+    likeCount,
+    commentCount,
+    userImage,
+    userHandle,
+    comments,
+  },
+}) => {
   const [open, setOpen] = useState(false);
 
   const closeDialog = () => {
@@ -41,7 +84,33 @@ const ScreamDialog = ({ classes, screamId, getScream, UI: { loading } }) => {
       <CircularProgress size={200} thickness={2} />
     </div>
   ) : (
-    <div>works</div>
+    <Grid container spacing={2}>
+      <Grid item sm={5}>
+        <img src={userImage} alt='Profile' className={classes.profileImage} />
+      </Grid>
+      <Grid item sm={7}>
+        <Typography
+          component={Link}
+          color='primary'
+          variant='h5'
+          to={`/users/${userHandle}`}
+        >
+          @{userHandle}
+        </Typography>
+        <hr className={classes.invisibleSeparator} />
+        <Typography variant='body2' color='textSecondary'>
+          {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
+        </Typography>
+        <hr className={classes.invisibleSeparator} />
+        <Typography variant='body1'>{body}</Typography>
+        <LikeButton screamId={screamId} />
+        <span>{likeCount} likes</span>
+        <MyButton tip='comments'>
+          <ChatIcon color='primary' />
+        </MyButton>
+        <span>{commentCount} comments</span>
+      </Grid>
+    </Grid>
   );
 
   return (
@@ -54,6 +123,13 @@ const ScreamDialog = ({ classes, screamId, getScream, UI: { loading } }) => {
         <UnfoldMoreIcon color='primary' />
       </MyButton>
       <Dialog open={open} onClose={closeDialog} fullWidth maxWidth='sm'>
+        <MyButton
+          tip='Close'
+          onClick={closeDialog}
+          tipClassName={classes.closeButton}
+        >
+          <CloseIcon />
+        </MyButton>
         <DialogContent>{dialogMarkup}</DialogContent>
       </Dialog>
     </Fragment>
@@ -74,6 +150,7 @@ ScreamDialog.propTypes = {
   scream: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
   getScream: PropTypes.func.isRequired,
+  screamId: PropTypes.string.isRequired,
 };
 
 export default connect(
