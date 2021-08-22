00a51c44d247a21be7fb32ffe6d2ef91faa0acc7
juanpchica <juanpchica@hotmail.com>
2021-08-22 15:32:48 -0500

    Adding styles

--- src/components/scream/Comments.js
+++ src/components/scream/Comments.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+const styles = (theme) => ({
+  ...theme.spreatThis,
+  commentImage: {
+    maxWidth: "100%",
+    height: 100,
+    objectFit: "cover",
+    borderRadius: "50%",
+  },
+  commentData: {
+    marginLeft: 20,
+  },
+});
+
 const Comments = () => {
   return <div>Comments</div>;
 };
