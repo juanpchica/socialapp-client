1a1cb563437cbd68521c59d0c17a762f9343a00e
juanpchica <juanpchica@hotmail.com>
2021-08-22 15:37:26 -0500

    Adding info the comment inside MUI components html

--- src/components/scream/Comments.js
+++ src/components/scream/Comments.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Link } from "react-router-dom";
@@ -22,11 +22,52 @@ const styles = (theme) => ({
 });
 
 const Comments = ({ comments, classes }) => {
-  return <div>Comments</div>;
+  return (
+    <Grid container>
+      {comments.map((comment, index) => {
+        const { body, createdAt, userImage, userHandle } = comment;
+        return (
+          <Fragment key={createdAt}>
+            <Grid item sm={12}>
+              <Grid container>
+                <Grid item sm={2}>
+                  <img
+                    src={userImage}
+                    alt='comment'
+                    className={classes.commentImage}
+                  />
+                </Grid>
+                <Grid item sm={9}>
+                  <div className={classes.commentData}>
+                    <Typography
+                      variant='h5'
+                      component={Link}
+                      to={`/users/${userHandle}`}
+                      color='primary'
+                    >
+                      {userHandle}
+                    </Typography>
+                    <Typography variant='body2' color='textSecondary'>
+                      {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
+                    </Typography>
+                    <hr className={classes.invisibleSeparator} />
+                    <Typography variabnt='body1'>{body}</Typography>
+                  </div>
+                </Grid>
+              </Grid>
+            </Grid>
+            {index !== comments.length - 1 && (
+              <hr className={classes.visibleSeparator} />
+            )}
+          </Fragment>
+        );
+      })}
+    </Grid>
+  );
 };
 
 Comments.propTypes = {
   comments: PropTypes.array.isRequired,
 };
 
-export default Comments;
+export default withStyles(styles)(Comments);
