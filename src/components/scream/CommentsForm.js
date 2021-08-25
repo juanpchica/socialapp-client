28bba0a6dc0200f48f2293ed47f7818ad03504d1
juanpchica <juanpchica@hotmail.com>
2021-08-24 22:17:45 -0500

    Adding redux to the commentForm component with its basic html structure

--- src/components/scream/CommentsForm.js
+++ src/components/scream/CommentsForm.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+// MUI Stuff
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
 
-const CommentsForm = () => {
-  return <div>Comments form</div>;
+// Redux stuff
+import { connect } from "react-redux";
+
+const styles = (theme) => ({
+  ...theme,
+});
+
+class CommentForm extends Component {
+  state = {
+    body: "",
+    errors: {},
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.UI.errors) {
+      this.setState({ errors: nextProps.UI.errors });
+    }
+    if (!nextProps.UI.errors && !nextProps.UI.loading) {
+      this.setState({ body: "" });
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.submitComment(this.props.screamId, { body: this.state.body });
+  };
+
+  render() {
+    const { classes, authenticated } = this.props;
+    const errors = this.state.errors;
+
+    const commentFormMarkup = authenticated ? (
+      <Grid item sm={12} style={{ textAlign: "center" }}>
+        <hr className={classes.visibleSeparator} />
+      </Grid>
+    ) : null;
+    return commentFormMarkup;
+  }
+}
+
+CommentForm.propTypes = {
+  submitComment: PropTypes.func.isRequired,
+  UI: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  screamId: PropTypes.string.isRequired,
+  authenticated: PropTypes.bool.isRequired,
 };
 
-export default CommentsForm;
+const mapStateToProps = (state) => ({
+  UI: state.UI,
+  authenticated: state.user.authenticated,
+});
+
+export default connect(mapStateToProps, { submitComment })(
+  withStyles(styles)(CommentForm)
+);
