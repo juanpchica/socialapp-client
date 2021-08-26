d738243338fd40ed1e3eca6b836e3b3e9a0b20b9
juanpchica <juanpchica@hotmail.com>
2021-08-25 23:07:50 -0500

    Adding submitComment action creator

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -82,6 +82,25 @@ export const unlikeScream = (screamId) => (dispatch) => {
     });
 };
 
+// Submit a comment
+export const submitComment = (screamId, commentData) => (dispatch) => {
+  axios
+    .post(`/scream/${screamId}/comment`, commentData)
+    .then((res) => {
+      dispatch({
+        type: SUBMIT_COMMENT,
+        payload: res.data,
+      });
+      dispatch(clearErrors());
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+};
+
 export const deleteScream = (screamId) => (dispatch) => {
   axios
     .delete(`/scream/${screamId}`)
