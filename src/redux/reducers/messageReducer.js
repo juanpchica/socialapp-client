99cd02f7daf2032cd9c4e95ec2e161459c3d74bf
juanpchica <juanpchica@hotmail.com>
2021-09-05 20:08:13 -0500

    Adding new list

--- src/redux/reducers/messageReducer.js
+++ src/redux/reducers/messageReducer.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const messageReducer = () => {
-  return <div>New component</div>;
+  return (
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+      <li>Item 4</li>
+    </ul>
+  );
 };
 
 export default messageReducer;
